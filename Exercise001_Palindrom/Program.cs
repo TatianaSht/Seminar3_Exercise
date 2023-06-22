0bc4646913dd7272cbb2c9dae9486803385a0438
tatiana_sht <tatianaorlova0271@gmail.com>
2023-06-23 01:21:56 +1000

    Added solution

---
+++ Exercise001_Palindrom/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+string strNumber = Console.ReadLine();
+
+if (strNumber.Length < 5 | strNumber.Length > 5 )
+{
+    Console.WriteLine("Ошибка! Введено не пятизначное число, попробуйте еще раз.");
+}
+else if (strNumber[0] == strNumber[4] && strNumber[1] == strNumber[3])
+{
+    Console.WriteLine("Число палиндром");
+}
+else
+{
+     Console.WriteLine("Число не палиндром");
+}
---
+++ Exercise002_VectorCoordinates/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними 
+// в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int[] coordinateA = new int [3];
+Console.WriteLine("Введите координату Х первой точки: ");
+coordinateA[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки: ");
+coordinateA[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z первой точки: ");
+coordinateA[2] = Convert.ToInt32(Console.ReadLine());
+
+int[] coordinateB = new int [3];
+Console.WriteLine("Введите координату Х второй точки: ");
+coordinateB[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y второй точки: ");
+coordinateB[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z второй точки: ");
+coordinateB[2] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(coordinateB[0] - coordinateA[0], 2) + Math.Pow(coordinateB[1] - coordinateA[1], 2) + Math.Pow(coordinateB[2] - coordinateA[2], 2)),2));
